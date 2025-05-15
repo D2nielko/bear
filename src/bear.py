d7036b8748e1ff3801f9da052d003ff4d2b6e63d
Daniel Ko <dannieko02@gmail.com>
2025-05-15 14:23:52 +0000

    Made brief changes to make file executable

--- src/bear.py
+++ src/bear.py
@@ -3,10 +3,12 @@ import socketio
 import requests
 import getpass 
 import os
+import langchain_anthropic
 
 API_KEY = os.environ.get("ANTHROPIC_API_KEY")
 
 app = FastAPI()
+# change the origin from * to specific domain after testing
 sio = socketio.AsyncServer(cors_allowed_origins="*")
 app_socket = socketio.ASGIApp(sio, app)
 
@@ -16,14 +18,13 @@ llm = ChatAnthropic(
     model="claude-3-haiku-20240307",
     temperature=0.7,
     max_tokens=200, 
-    api_key= API_KEY
+    api_key= API_KEY,
 )
 
-
-async def receive_message(data):
+def receive_message(data):
     print("message received: {data}")
 
-    user_message = data["message"]
+#    user_message = data["message"]
     
 
     messages=[
@@ -32,10 +33,12 @@ async def receive_message(data):
         {"role": "assistant", "content": "Good morning! I feel like today is going to be an awesome day! Can you give me head pats please?"},
         {"role": "user", "content": "Good night bear bear"},
         {"role": "assistant", "content": "Good night, I am sleepy now. Please hug me before you go to bed!"},
-        {"role": "user", "content": user_message }
+        {"role": "user", "content": data }
     ]
 
     ai_msg = llm.invoke(messages)
 
 
     print(ai_msg)
+
+receive_message("hello, I am going to sleep now")
